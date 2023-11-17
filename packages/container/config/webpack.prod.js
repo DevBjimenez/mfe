b81a3f64769cb03f99a937de26e27b95680c2679
Brayan Jimenez <bjimenez@MacBook-Pro-de-Brayan-2.local>
2023-11-17 09:03:18 -0500

    created container deploy script

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const ModuleFederationPlugin = require('webpack').container.ModuleFederationPlugin
+const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
+const { default: merge } = require('webpack-merge')
+
+const domain = process.env.PRODUCTION_DOMAIN
+
+const configProd = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                marketingApp: `marketing@${domain}/marketing/remoteEntry.js`
+            },
+            shared: packageJson.dependencies
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, configProd)
---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const ModuleFederationPlugin = require('webpack').container.ModuleFederationPlugin
+const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
+const { default: merge } = require('webpack-merge')
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingIndex': './src/bootstrap'
+            },
+            shared: packageJson.dependencies
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, prodConfig)
