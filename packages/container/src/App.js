0e6e2eda9e79ec3a80dd373e61e49e72cbd82ac8
Brayan Jimenez <bjimenez@MacBook-Pro-de-Brayan-2.local>
2023-11-18 08:02:40 -0500

    randomize css classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import { BrowserRouter } from 'react-router-dom'
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
+
+
 const App = () => {
-    return <BrowserRouter>
-        <section>
-            <Header></Header>
-            <MarketingApp></MarketingApp>
-        </section>
-    </BrowserRouter>
+    return <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+            <section>
+                <Header></Header>
+                <MarketingApp></MarketingApp>
+            </section>
+        </BrowserRouter>
+    </StylesProvider>
+
 
 }
 export default App
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma'
+})
 
 import Landing from './components/Landing'
 import Princing from './components/Pricing'
 const App = () => {
     return <div>
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassName}>
             <BrowserRouter>
                 <Switch>
                     <Route exact path="/princing" component={Princing} />
